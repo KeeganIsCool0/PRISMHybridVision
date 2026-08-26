bf11637898eb73658790929f337c79e9a2cce4ad
Keegan <kvstudio10@gmail.com>
2026-08-26 11:12:05 -0500

    Add files via upload

---
+++ Firmware/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/kvitale/ReconsoleApp
+# Build directory: /home/kvitale/ReconsoleApp/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test([=[audio-engine-test]=] "/home/kvitale/ReconsoleApp/build/audio-engine-test")
+set_tests_properties([=[audio-engine-test]=] PROPERTIES  _BACKTRACE_TRIPLES "/home/kvitale/ReconsoleApp/CMakeLists.txt;26;add_test;/home/kvitale/ReconsoleApp/CMakeLists.txt;0;")
